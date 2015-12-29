a5a6f9b81835fb6bdb0c40137569c1d74aff1a07
Charles Zahn <Charles.Zahn@chloeandisabel.com>
2015-12-29 12:20:11 -0500

    remove style attributes

--- lib/recaptcha/client_helper.rb
+++ lib/recaptcha/client_helper.rb
@@ -23,21 +23,15 @@ module Recaptcha
       if options[:noscript] != false
         html << <<-HTML
           <noscript>
-            <div style="width: 302px; height: 352px;">
-              <div style="width: 302px; height: 352px; position: relative;">
-                <div style="width: 302px; height: 352px; position: absolute;">
-                  <iframe
-                    src="#{fallback_uri}"
-                    frameborder="0" scrolling="no"
-                    style="width: 302px; height:352px; border-style: none;">
+            <div>
+              <div>
+                <div>
+                  <iframe src="#{fallback_uri}" frameborder="0" scrolling="no">
                   </iframe>
                 </div>
-                <div style="width: 250px; height: 80px; position: absolute; border-style: none;
-                  bottom: 21px; left: 25px; margin: 0px; padding: 0px; right: 25px;">
+                <div>
                   <textarea id="g-recaptcha-response" name="g-recaptcha-response"
-                    class="g-recaptcha-response"
-                    style="width: 250px; height: 80px; border: 1px solid #c1c1c1;
-                    margin: 0px; padding: 0px; resize: none;" value="">
+                    class="g-recaptcha-response" value="">
                   </textarea>
                 </div>
               </div>
